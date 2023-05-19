c4868fba8b3c6de7286fc6a20169306d4cd34292
yeti-s <sel9371@gmail.com>
2023-05-20 02:25:17 +0900

    refactor: remove electron dependency

---
+++ express/main.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+// const app = express();
+
+// const PORT = 3003;
+
+// function handleListen () {
+//   console.log("Listen on 3000 port");
+// }
+
+// function handleHome (req, res) {
+//   console.log('Home');
+// }
+
+// app.get('/', handleHome)
+
+// app.listen(PORT, handleListen);
