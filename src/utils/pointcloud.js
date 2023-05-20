b3262feb9fcddcd14b9f6696f882eae0f8147e83
yeti-s <sel9371@gmail.com>
2023-05-20 22:30:08 +0900

    feat: load and remove pointcloud

--- src/utils/pointcloud.js
+++ src/utils/pointcloud.js
@@ -1,51 +1,77 @@
 import EventManager from "./EventManager";
 
-const Potree = window.Potree;
-const Manager = new EventManager();
+const POTREE = window.Potree;
+const MANAGER = new EventManager();
 const REF = {
     isInit: false,
-    pointclouds: {}
+    rendererId: ''
+    // pointclouds: {},
 };
+
+// EVENT NAME
+const ADD_POINTCLOUD = "add_pointcloud";
+const REMOVE_POINTCLOUD= "remove_pointcloud";
+// const REMOVE_POINTCLOUD_ALL = "remove_pointcloud_all";
 
 
 const isInit = () => REF.isInit;
-const getViewer = () => REF.viewer;
-// const getCanvas = () => REF.canvas;
-// const getScene = () => REF.scene;
-// const getCamera = () => REF.camera;
-// const getRenderer = () => REF.renderer;
-// const getControls = () => REF.controls;
 
 
-// const render = () => {
-//     getControls().update();
-//     getRenderer().render(getScene(), getCamera());
-//     requestAnimationFrame(render);
-// }
-
-export const initPotree = id => {
+export const init = id => {
     if (isInit()) return;
 
     const renderArea = document.getElementById(id);
-    const viewer = new Potree.Viewer(renderArea, {});
+    const viewer = new POTREE.Viewer(renderArea, {});
     viewer.setEDLEnabled(true);
     viewer.setFOV(60);
     viewer.setPointBudget(3*1000*1000);
     viewer.setMinNodeSize(0);
 
-    REF.isInit = true;
-    REF.viewer = viewer;
 
-    createPointcloud('/file?path=/home/yeti/pointcloud_manipulator/express/total_converted/metadata.json',"test");
+    REF.isInit = true;
+    REF.rendererId = id;
+    const pointclouds = {};
+    const scene = viewer.measuringTool.scene;
+    const pcdScene = viewer.scene.scenePointCloud;
+    const view = viewer.scene.view;
+
+
+    MANAGER.register(ADD_POINTCLOUD, (pcd) => {
+        const uuid = pcd.uuid;
+        
+        const eventId = MANAGER.register(REMOVE_POINTCLOUD, (id) => {
+            if (id !== uuid) return;
+            pcdScene.remove(pcd);
+            delete pointclouds[uuid];
+            MANAGER.unregister(eventId);
+        })
+        
+        pointclouds[uuid] = pcd;
+        viewer.scene.addPointCloud(pcd);
+        viewer.zoomTo(pcd);
+    })
 }
 
-export const createPointcloud = (path, name) => {
-    Potree.loadPointCloud(path, name, res=>{
-        let viewer = getViewer();
-        viewer.scene.addPointCloud(res.pointcloud);
-        console.log(res);
+// export const destroy = () => {
+//     let rendererDom = document.getElementById(REF.rendererId);
+//     let children = rendererDom.childNodes;
+//     for (let i = children.length - 1; i >= 0; i--)
+//         rendererDom.remove(children[i])
 
-        viewer.scene.view.position.set(-500, -500, 100);
-        viewer.scene.view.lookAt(-500, -500, 0);
+//     REF.isInit = false;
+//     MANAGER.reset();
+// }
+
+export const loadPointCloud = (path, name = "noname") => {
+    return new Promise((resolve) => {
+        POTREE.loadPointCloud(path, name, result=>{
+            const pcd = result.pointcloud;
+            MANAGER.notify(ADD_POINTCLOUD, pcd);
+            resolve(pcd.uuid);
+        })
     })
-} 
+}
+
+export const removePointcloud = (id) => {
+    MANAGER.notify(REMOVE_POINTCLOUD, id);
+}
