c4e193d958e45a5c44071a654cfd8b5f76385cdc
yeti-s <sel9371@gmail.com>
2023-05-21 12:15:34 +0900

    style: set mui theme primary

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Theme } from './theme';
+import { ThemeProvider } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+
+export const Theme = createTheme({
+    palette: {
+        primary: {
+            main: '#f84afb'
+        }
+    }
+})
