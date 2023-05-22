13b7a443a128a4c7ca0175769907b6aeee28a1e0
yeti-s <sel9371@gmail.com>
2023-05-22 23:03:49 +0900

    feat: process multiple request

--- clib/lasManipC.cpp
+++ clib/lasManipC.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
+#include <vector>
 
 #include <liblas/liblas.hpp>
 #include <opencv4/opencv2/core.hpp>
@@ -8,7 +9,249 @@
 #include <nlohmann/json.hpp>
 
 using namespace std;
-using json = nlohmann::json;
+
+
+string formatNumber(int number, int unit);
+
+class ClipBox {
+private:
+    // created from constructor
+    string outPath;
+    cv::Mat matrix;
+    uint16_t threshold;
+    bool writeLabelLas;
+
+    // clipping tools
+    ofstream *outLas;
+    ofstream *labelLas;
+    liblas::Writer *lasWriter;
+    liblas::Writer *labelWriter;
+    double cmin[3] = {DBL_MAX, DBL_MAX};
+    double cmax[3] = {DBL_MIN, DBL_MIN};
+
+    // craated after clipping
+    uint numPoints = 0;
+    uint numLabelPoints = 0;
+    double min[3] = {0, 0, DBL_MAX};
+    double max[3] = {0, 0, DBL_MIN};
+
+public:
+    ClipBox(string outPath, double *elements, uint16_t threshold, bool writeLabelLas) {
+        this->outPath = outPath;
+        this->threshold = threshold;
+        this->writeLabelLas = writeLabelLas;
+
+        this->matrix = cv::Mat::zeros(4, 4, CV_64F);
+        for (int i = 0; i < 16; i++) {
+            this->matrix.at<double>((i/4), (i%4)) = elements[i];
+        }
+        this->matrix = this->matrix.inv().t();
+    }
+    ~ClipBox() {
+        if (this->lasWriter != nullptr) {
+            delete this->lasWriter;
+            delete this->outLas;
+            if (this->labelWriter != nullptr) {
+                delete this->labelWriter;
+                delete this->labelLas;
+            }
+        }
+    }
+
+    void setLasHeader(liblas::Header header) {
+        this->outLas = new ofstream(this->outPath + ".las", std::ios::binary);
+        this->lasWriter = new liblas::Writer(*this->outLas, header);
+
+        if (this->writeLabelLas) {
+            this->labelLas = new ofstream(this->outPath + "_label.las", std::ios::binary);
+            this->labelWriter = new liblas::Writer(*this->labelLas, header);
+        }
+    }
+
+    void addPoint(liblas::Point point) {
+        double x = point.GetX();
+        double y = point.GetY();
+        double z = point.GetZ();
+        double intensity = point.GetIntensity();
+
+        cv::Vec4d pointVector(x, y, z, 1.0);
+        cv::Mat result = this->matrix * cv::Mat(pointVector);
+        double cx = result.at<double>(0, 0);
+        double cy = result.at<double>(1, 0);
+        double cz = result.at<double>(2, 0);
+
+        if (cx <= 0.5 && cx >= -0.5 && cy <= 0.5 && cy >= -0.5 && cz <= 0.5 && cz >= -0.5) // inner point
+        {
+            this->numPoints++;
+            this->lasWriter->WritePoint(point);
+            if (intensity >= this->threshold)
+            {
+                this->numLabelPoints++;
+                if (this->writeLabelLas) this->labelWriter->WritePoint(point);
+
+                if (cx < this->cmin[0]) {
+                    this->cmin[0] = cx;
+                    this->min[0] = x;
+                }
+                if (cx > this->cmax[0]) {
+                    this->cmax[0] = cx;
+                    this->max[0] = x;
+                }
+                if (cy < this->cmin[1]) {
+                    this->cmin[1] = cy;
+                    this->min[1] = y;
+                }
+                if (cy > this->cmax[1]) {
+                    this->cmax[1] = cy;
+                    this->max[1] = y;
+                }
+                if (z < this->min[2]) this->min[2] = z;
+                if (z > this->max[2]) this->max[2] = z;
+            }
+        }
+
+        
+    }
+
+    nlohmann::json exportData() {
+        nlohmann::json json;
+        json["intensity"] = this->threshold;
+        json["num_points"] = this->numPoints;
+        json["num_label_points"] = this->numLabelPoints;
+        json["min"] = {this->min[0], this->min[1], this->min[2]};
+        json["max"] = {this->max[0], this->max[1], this->max[2]};
+
+        double elements[16];
+        for (int i = 0; i < 16; i++) {
+            elements[i] = this->matrix.at<double>((i/4), (i%4));
+        }
+        json["matrix"] = elements;
+
+        return json;
+    }
+
+
+};
+
+class ClipTask
+{
+private:
+    nlohmann::json json;
+    filesystem::path lasPath;
+    filesystem::path dir;
+    vector<ClipBox> boxes;
+
+public:
+    ClipTask(nlohmann::json json)
+    {
+        this->json = json;
+        this->lasPath = filesystem::path((string)json["lasPath"]);
+        this->dir = lasPath.parent_path();
+
+        nlohmann::json boxes = json["boxes"];
+        for (int i = 0; i < boxes.size(); i++) {
+            nlohmann::json box = boxes[i];
+            nlohmann::json matrix = box["matrix"];
+
+            uint16_t intensity = box["intensity"];
+            double elements[16];
+            for (int j = 0; j < 16; j++) {
+                elements[j] = matrix[j];
+            }
+
+            filesystem::path outPath = this->dir / formatNumber(i, 6);
+            this->boxes.emplace_back(outPath.string(), elements, intensity, true);
+        }
+    }
+
+    void clip()
+    {
+        try {
+            ifstream las(this->lasPath, ios::in | ios::binary);
+            liblas::Reader reader(las);
+            liblas::Header header = reader.GetHeader();
+            for (auto it = this->boxes.begin(); it != this->boxes.end(); it++) {
+                it->setLasHeader(header);
+            }
+
+            uint numPoints = header.GetPointRecordsCount();
+            uint i = 0;
+            while(reader.ReadNextPoint()) {
+                if ((i+1) % 100000 == 0) 
+                    cout << "(" << i+1 << "/" << numPoints << ") processing." << endl;
+
+                auto point = reader.GetPoint();
+                for (auto it = this->boxes.begin(); it != this->boxes.end(); it++) {
+                    it->addPoint(point);
+                }
+                i++;
+            }
+        } catch(exception &e)
+        {
+            cerr << "LAS 파일 변환 실패: " << e.what() << endl;
+            exit(1);
+        }
+    }
+
+    void exportData() {
+        vector<nlohmann::json> boxes;
+        for (auto it = this->boxes.begin(); it != this->boxes.end(); it++) {
+            boxes.push_back(it->exportData());
+        }
+        this->json["boxes"] = boxes;
+
+        filesystem::path outpath = this->dir / "clip.json";
+        ofstream outFile(outpath);
+        outFile << std::setw(4) << this->json << std::endl;
+    }
+};
+
+
+// liblas::Writer *test(liblas::Header header, liblas::Point point) {
+//     ofstream *outlas = new ofstream("/mnt/c/Users/yeti/Documents/github/data/paris/las/test.las", std::ios::binary);
+//     liblas::Writer *writer = new liblas::Writer(*outlas, header);
+//     writer->WritePoint(point);
+
+//     return writer;
+// }
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        cout << "input your json file!" << endl;
+        return 1;
+    }
+
+    // liblas::Header header;
+    // liblas::Point point(&header);
+    // auto writer = test(header, point);
+    // writer->WritePoint(point);
+
+
+    string jsonPath(argv[1]);
+    ifstream jsonFile(jsonPath);
+    nlohmann::json data = nlohmann::json::parse(jsonFile);
+    ClipTask clipTask(data);
+    clipTask.clip();
+    clipTask.exportData();
+}
+
+string formatNumber(int number, int unit)
+{
+    std::string numberStr = std::to_string(number);
+    int numZeros = unit - numberStr.length();
+
+    if (numZeros > 0)
+    {
+        std::string zeros(numZeros, '0');
+        numberStr = zeros + numberStr;
+    }
+
+    return numberStr;
+}
+
+
 
 /*
 json format
@@ -25,97 +268,3 @@ json format
     ]
 }
 */
-string formatNumber(int number, int unit);
-int clipLas(string lasPath, string outPath, cv::Mat matrix, int inetensity);
-
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        cout << "input your json file!" << endl;
-        return 1;
-    }
-
-    string jsonPath(argv[1]);
-    ifstream jsonFile(jsonPath);
-    json data = json::parse(jsonFile);
-
-    filesystem::path lasPath(data["lasPath"]);
-    json boxes = data["boxes"];
-    for (int i = 0; i < boxes.size(); i++) {
-        json box = boxes[i];
-        json elements = box["matrix"];
-        int intensity = box["intensity"];
-
-        cv::Mat matrix = cv::Mat::zeros(4, 4, CV_64F);
-        for (int j = 0; j < 16; j++) {
-            double element = elements[j];
-            matrix.at<double>(j/4, j%4) = element;
-        }
-
-        filesystem::path outPath = lasPath.parent_path();
-        outPath += "/box";
-        outPath += formatNumber(i, 3);
-        outPath += ".las";
-        clipLas(lasPath.string(), outPath.string(), matrix.inv().t(), intensity);
-    }
-    // cout << boxes.size() << endl;
-    // double *elements = data[""]
-}
-
-std::string formatNumber(int number, int unit) {
-    std::string numberStr = std::to_string(number);
-    int numZeros = 3 - numberStr.length();
-
-    if (numZeros > 0) {
-        std::string zeros(numZeros, '0');
-        numberStr = zeros + numberStr;
-    }
-
-    return numberStr;
-}
-
-
-int clipLas(string lasPath, string outPath, cv::Mat matrix, int inetensity)
-{
-    string inputFilePath = "/mnt/c/Users/yeti/Documents/github/data/paris/las/Lille_0.las";  // 입력 바이너리 파일 경로
-    string outputFilePath = "/mnt/c/Users/yeti/Documents/github/data/paris/las/0_test.las";  // 출력 LAS 파일 경로
-
-    try {
-        ifstream inputFile(inputFilePath, ios::in | ios::binary);
-        liblas::Reader reader(inputFile);
-        liblas::Header header = reader.GetHeader();
-        std::ofstream outputFile(outputFilePath, std::ios::binary);  // 출력 파일 스트림 생성
-        liblas::Writer writer(outputFile, reader.GetHeader());  // 출력 파일 스트림을 사용하여 Writer 생성
-
-        int num = 0;
-
-        while(reader.ReadNextPoint()) {
-            liblas::Point point = reader.GetPoint();
-
-            double x = point.GetX();
-            double y = point.GetY();
-            double z = point.GetZ();
-
-            cv::Vec4d vec(x, y, z, 1.0);
-            cv::Mat result = matrix * cv::Mat(vec);
-
-            double cx = result.at<double>(0, 0);
-            double cy = result.at<double>(1, 0);
-            double cz = result.at<double>(2, 0);
-
-            if (cx <= 0.5 && cx >= -0.5 && cy <= 0.5 && cy >= -0.5 && cz <= 0.5 && cz >= -0.5) {
-                // cout << "found : " << cx << ",  " << cy << ",  " << cz  << endl;
-                num++;
-                writer.WritePoint(point);
-            }
-
-            // writer.WritePoint(point);
-        }
-
-        cout << "found num : "<< num << endl;
-    } catch (exception& e) {
-        cerr << "LAS 파일 변환 실패: " << e.what() << endl;
-        return 1;
-    }
-
-    return 0;
-}
